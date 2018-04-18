2f6ab347d4b37441dec8d7a6055a34019c222ca6
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-18 15:33:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,5 @@
 
 def countdown(number)
-  
   until number == 0
     number -= 1
     puts '#{number} SECOND(S)!'
@@ -9,5 +8,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep
-  
+  until number == 0
+    sleep(1)
+    number -= 1
+    puts '#{number} SECOND(S)!'
+  end
+  "HAPPY NEW YEAR!"
 end
